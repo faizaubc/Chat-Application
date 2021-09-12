f96cc04369bf8acec44334f66c3a9cd1db8dee05
Faiza Yahya <faizayahya.jp@gmail.com>
2021-09-12 16:09:07 -0600

    Commit comments

--- Chat Application/Chat Application.cpp
+++ Chat Application/Chat Application.cpp
@@ -1,12 +1,37 @@
 // Chat Application.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include "pch.h"
 #include <iostream>
+#include <WS2tcpip.h> //windows sockets uses this api to access the network
+//it includes winsock network
+#pragma comment (lib, "ws2_32.lib") //tells the linker to add the 'libname' library to the list
+//of library dependencies as if you had added it in the project properties at Linker -> 
+//Input-> Additional dependencies
+using namespace::std;
 
 int main()
 {
-    std::cout << "Hello World  !!\n"; 
+    cout << "Hello World  !!\n";
+	//initialize winsock = windows socket
+	WSADATA wsData;
+
+	//create a socket one thta we are binding to
+	//creating a socket has a domain IPv4 protocol or IPv6 protocol
+	//The IPv6 is the sixth revision to the Internet Protocol and sucessor to IPv4
+	//It provides unique addresses necessary for internet-enabled devices to communicate
+	//It also asks for a socket type: the communication type 
+
+	//bind the socket to an ip address and port
+
+	//wait for a connectionn
+
+	//close listening socket
+
+	//while loop: accept and echo message back to client
+
+	//close the socket
+
+	//shutdown winsock
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
